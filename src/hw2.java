2adf7ee854e3bc9669bf2c9f3b4a5bf100896597
Mümin <mozdamar90@hotmail.com>
2022-07-13 18:38:58 +0300

    Initial commit

---
+++ src/hw2.java
@@ -0,0 +1,12 @@
+public class hw2 {
+    public static void main(String[] args) {
+        int esit1 = 3;
+        int esit2 = 3;
+        int esit3 = 4;
+        System.out.println("esit1 ="+1);
+        System.out.println("esit2 ="+2);
+        System.out.println("esit3 ="+3);
+        System.out.println("esit1 esittir esit2="+(esit1 == esit2));
+        System.out.println("esit2 esitdegildir esit3"+(esit2 != esit3));
+    }
+}
---
+++ src/hw2carpımTablosu.java
@@ -0,0 +1,24 @@
+public class hw2carpımTablosu {
+    public static void main(String[] args) {
+
+        int c =1;
+        int c2 =2;
+        int c3 =3;
+        int c4 =4;
+        int c5 =5;
+        int c6 =6;
+        int c7 =7;
+        int c8 =8;
+        int c9 =9;
+        System.out.println("1*1= "+c);
+        System.out.println("2*1= "+c2);
+        System.out.println("3*1= "+c3);
+        System.out.println("4*1= "+c4);
+        System.out.println("5*1= "+c5);
+        System.out.println("6*1= "+c6);
+        System.out.println("7*1= "+c7);
+        System.out.println("8*1= "+c8);
+        System.out.println("9*1= "+c9);
+
+    }
+}
